5d0f905e305dcf87dcaba9c744c45685379beb0a
Luke <133702477+kukel1dd@users.noreply.github.com>
2023-05-15 15:57:30 -0400

    Update car_maintenance.py

--- car_maintenance.py
+++ car_maintenance.py
@@ -18,22 +18,36 @@ class Car:
     def oil_change(self, Miles_Last_change):
         """a simple function to check how many miles until your next oil change."""
         self.next_change = 5000 - (self.current_mileage - Miles_Last_change)
-        
-        print(f'you have {self.next_change} miles until your next oil change. make sure to check your oil atleast a few times in the next few months!')
+        if Miles_Last_change > self.current_mileage:
+            print("\nyour current miles cant be less than your last change!")
+        elif self.next_change <= 0:
+            print("\nLooks like you need an oil change!")
+        else:
+            print(f'\nYou have {self.next_change} miles until your next oil change. make sure to check your oil atleast a few times in the next few months!')
         
     
     
     def estimate_days(self, miles_per_day):
         """A function to roughly estimate the weeks until your next change, based off of a normal day of driving."""
         days_till_change = self.next_change // miles_per_day 
-        print(f"you have roughly {days_till_change} until your next oil change.")
-        weeks = days_till_change // 7
-        print(f"roughly {weeks} weeks.")
+        if days_till_change <= 0:
+            print("\nYou need an oil change!")
+        else:
+            print(f"\nYou have roughly {days_till_change} days until your next oil change.")
+            weeks = days_till_change // 7
+            print(f"\nroughly {weeks} weeks.")
         
 
 
-# my_car = Car('nissan', 'sentra', 2015, 82148)
 
-# print(my_car.format_name())
-# my_car.oil_change(82016)
-# my_car.estimate_days(36)
+cars_make = input("Enter the make of your car: ")
+car_model = input('Enter the model of your car: ')
+car_year = input('Enter the year of your car: ')
+
+car_mileage = int(input('Enter your current mileage: '))
+my_car = Car(cars_make, car_model, car_year, car_mileage)
+print(my_car.format_name())
+car_last_chage = int(input("\nEnter your mileage at your last oil change:"))
+my_car.oil_change(car_last_chage)
+miles_a_day = int(input("Enter your miles of an average day of driving: "))
+my_car.estimate_days(miles_a_day)
